a5a98e19c8b64dc4f832356abe156e8a9d555d94
adiko <dikodam@googlemail.com>
2018-03-28 11:48:50 +0200

    day 2 complete

--- src/main/kotlin/de/dikodam/adventofkotlin/Day02.kt
+++ src/main/kotlin/de/dikodam/adventofkotlin/Day02.kt
@@ -1,14 +1,52 @@
 package de.dikodam.adventofkotlin
 
+import kotlin.math.min
+
 fun main(args: Array<String>) {
     AbstractDay.doTheMagicWithKotlin(Day02())
 }
-class Day02 : AbstractDay(){
+
+class Day02 : AbstractDay() {
     override fun task1() {
-        input.forEach { println(it) }
+        val sum = input.map(::parseDimensions)
+            .map(::computeArea)
+            .sum()
+        println("T1: area sum is $sum")
+    }
+
+    private fun parseDimensions(str: String): Triple<Int, Int, Int> {
+        val dimensions = str.split("x")
+            .map(String::toInt)
+        return Triple(dimensions[0], dimensions[1], dimensions[2])
+    }
+
+    private fun computeArea(dimensions: Triple<Int, Int, Int>): Int {
+        val area1 = dimensions.first * dimensions.second
+        val area2 = dimensions.first * dimensions.third
+        val area3 = dimensions.second * dimensions.third
+        val minArea = min(area1, min(area2, area3))
+        return 2 * (area1 + area2 + area3) + minArea
     }
 
     override fun task2() {
+        val ribbonLength = input.map(::parseDimensions)
+            .map(::computeRibbonLength)
+            .sum()
+
+        println("T3: ribbon length is $ribbonLength")
     }
 
+    private fun computeRibbonLength(dimensions: Triple<Int, Int, Int>): Int {
+        val perimeter1 = 2 * (dimensions.first + dimensions.second)
+        val perimeter2 = 2 * (dimensions.first + dimensions.third)
+        val perimeter3 = 2 * (dimensions.second + dimensions.third)
+
+        val minPerimeter = min(perimeter1, min(perimeter2, perimeter3))
+
+        val volume = dimensions.first * dimensions.second * dimensions.third
+
+        return minPerimeter + volume
+    }
 }
+
+
