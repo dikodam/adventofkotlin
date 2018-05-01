8ecfe9fc03ae8f5d4e68e669a71145fb6d0eac81
adiko <dikodam@googlemail.com>
2018-05-01 14:37:05 +0200

    day05.kt

---
+++ src/main/kotlin/de/dikodam/adventofkotlin/Day05.kt
@@ -0,0 +1,16 @@
+package de.dikodam.adventofkotlin
+
+fun main(args: Array<String>) {
+    Day05().doTheMagic()
+}
+
+class Day05 : AbstractDay() {
+    override fun task1() {
+        TODO("not implemented")
+    }
+
+    override fun task2() {
+        TODO("not implemented")
+    }
+
+}
