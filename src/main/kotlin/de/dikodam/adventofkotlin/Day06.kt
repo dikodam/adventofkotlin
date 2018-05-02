ce5c0308be9de327eba648747c3321538151df83
adiko <dikodam@googlemail.com>
2018-05-02 10:56:19 +0200

    Day06 structure

---
+++ src/main/kotlin/de/dikodam/adventofkotlin/Day06.kt
@@ -0,0 +1,13 @@
+package de.dikodam.adventofkotlin
+
+fun main(args: Array<String>) {
+    Day06().doTheMagic()
+}
+
+class Day06 : AbstractDay() {
+    override fun task1() {
+    }
+
+    override fun task2() {
+    }
+}
