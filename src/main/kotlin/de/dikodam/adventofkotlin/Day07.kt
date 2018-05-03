65102ef95cd80dd414fef3746841b8c41bf6ac64
adiko <dikodam@googlemail.com>
2018-05-03 10:29:52 +0200

    day 7 setup

---
+++ src/main/kotlin/de/dikodam/adventofkotlin/Day07.kt
@@ -0,0 +1,18 @@
+package de.dikodam.adventofkotlin
+
+fun main(args: Array<String>) {
+    Day07().doTheMagic()
+}
+
+class Day07 : AbstractDay() {
+    val input: List<String> = loadInput()
+
+    override fun task1() {
+
+    }
+
+    override fun task2() {
+    }
+
+
+}
