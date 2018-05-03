0e8191658908db9126abbefe6db2d84ae28e565b
adiko <dikodam@googlemail.com>
2018-05-03 10:10:54 +0200

    day 6 task 2 finished

--- src/main/kotlin/de/dikodam/adventofkotlin/Day06.kt
+++ src/main/kotlin/de/dikodam/adventofkotlin/Day06.kt
@@ -16,20 +16,14 @@ class Day06 : AbstractDay() {
 
     override fun task1() {
         val instructions = loadInput()
-            .map { parseLineToInstruction(it, ::parseCommandForTask1) }
-            .toList()
+            .map { parseLineToInstruction(it) }
 
         val lightbulbs = (0..999)
             .map {
                 (0..999).map { LightbulbOnOff() }
             }
 
-        instructions
-            .forEach { (command, coordsFrom, coordsTo) ->
-                (coordsFrom..coordsTo)
-                    .map { (x, y) -> lightbulbs[x][y] }
-                    .forEach { command(it) }
-            }
+        instructions.applyTo(lightbulbs)
 
         val count = lightbulbs.flatMap { it }
             .filter { bulb -> bulb.turnedOn }
@@ -38,8 +32,17 @@ class Day06 : AbstractDay() {
         println("task 1: $count bulbs are turned on after all instructions completed")
     }
 
-    private fun parseLineToInstruction(instructionLine: String, commandParser: (String) -> Command): Triple<Command, Coordinates, Coordinates> {
-        val command = commandParser(instructionLine)
+    fun List<Instruction>.applyTo(lightbulbs: List<List<Lightbulb>>) {
+        this
+            .forEach { (command, coordsFrom, coordsTo) ->
+                (coordsFrom..coordsTo)
+                    .map { (x, y) -> lightbulbs[x][y] }
+                    .forEach { command(it) }
+            }
+    }
+
+    private fun parseLineToInstruction(instructionLine: String): Triple<Command, Coordinates, Coordinates> {
+        val command = parseCommand(instructionLine)
         val coordinates = instructionLine.dropWhile { it !in '0'..'9' }
             .split(" through ")
             .map { parseCoordinates(it) }
@@ -47,7 +50,7 @@ class Day06 : AbstractDay() {
         return Instruction(command, coordinates[0], coordinates[1])
     }
 
-    private fun parseCommandForTask1(instructionLine: String): Command {
+    private fun parseCommand(instructionLine: String): Command {
         return when {
             instructionLine.startsWith("toggle") -> { bulb: Lightbulb -> bulb.toggle() }
             instructionLine.startsWith("turn on") -> { bulb: Lightbulb -> bulb.turnOn() }
@@ -66,6 +69,20 @@ class Day06 : AbstractDay() {
     }
 
     override fun task2() {
+        val instructions = loadInput()
+            .map { parseLineToInstruction(it) }
+
+        val lightbulbs = (0..999)
+            .map { (0..999).map { LightbulbWithBrightness() } }
+
+        instructions.applyTo(lightbulbs)
+
+        val totalBrightness = lightbulbs
+            .flatMap { it }
+            .map { it.brightness }
+            .sum()
+
+        println("task 2: total brightness is $totalBrightness")
     }
 }
 
